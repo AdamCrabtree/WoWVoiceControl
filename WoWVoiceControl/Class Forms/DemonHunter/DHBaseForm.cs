d92841b4c5d2cf22a7f76ffc168b9c4372a52ff7
rambopfc <JoeBotta@gmail.com>
2017-05-27 00:20:58 -0400

    Demon Hunter Added
    Added forms and abilties for DH. Can build and run, no idea if it
    actually works ;)

---
+++ WoWVoiceControl/Class Forms/DemonHunter/DHBaseForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WoWVoiceControl.Class_Forms.DemonHunter.Specs;
+using WoWVoiceControl.SpecForms;
+
+namespace WoWVoiceControl
+{
+    public partial class DHBaseForm : Form
+    {
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        DHHotkeys hotkeyList;
+        public DHBaseForm()
+        {
+            InitializeComponent();
+        }
+        public DHBaseForm(DHHotkeys hotkeyList)
+        {
+            this.hotkeyList = hotkeyList;
+            InitializeComponent();
+        }
+
+        private void bVengeance_Click(object sender, EventArgs e)
+        {
+            VengeanceHotkeysForm hotkeyForm = new VengeanceHotkeysForm();
+            hotkeyForm.Show();
+            this.Hide();
+        }
+
+        private void bHavoc_Click(object sender, EventArgs e)
+        {
+            HavocHotkeysForm hotkeyForm = new HavocHotkeysForm();
+            hotkeyForm.Show();
+            this.Hide();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+            Process p = Process.GetProcessesByName("Wow-64").FirstOrDefault();
+            IntPtr h = p.MainWindowHandle;
+            recEngine.LoadGrammarAsync(DHGrammar.GenerateDHGrammar());
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        private void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string key;
+            hotkeyList.DHHotkeyDictionary.TryGetValue(e.Result.Text, out key);
+            SendKeys.Send(key);
+            SendKeys.Send("+2");
+        }
+    }
+}
